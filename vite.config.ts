2987fa669046c38c2ec822180db6f7a077fd97dd
Brian Schultz <brian.schultz@genesys.com>
2026-04-19 15:18:23 -0400

    update to use docs directory

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/markwright/",
   plugins: [react()],
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
 });
